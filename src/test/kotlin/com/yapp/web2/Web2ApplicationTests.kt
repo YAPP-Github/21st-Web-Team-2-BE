0fc50de896c7ba04c36aeb2a71c1327ce88bc619
limjimin <dkdldsla0000@naver.com>
2022-11-27 12:39:28 +0900

    :tada: 프로젝트 생성

---
+++ src/test/kotlin/com/yapp/web2/Web2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yapp.web2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Web2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
