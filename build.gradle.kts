c2fac6ea64d69be8c072fd2cfb4bd30eaf8e8cab
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-11-27 23:31:40 +0900

    :wrench: aws parameter store 접근 권한 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.coveo:spring-boot-parameter-store-integration:1.5.0")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
