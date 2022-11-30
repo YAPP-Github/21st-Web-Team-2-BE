46f1ca8a4ed26060caad18feba5710bb08117b9d
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-11-30 15:33:11 +0900

    :test_tube: Rest Docs를 검증할 테스트를 추가합니다

---
+++ src/test/kotlin/com/yapp/web2/controller/docs/RestDocsControllerTest.kt
@@ -0,0 +1,12 @@
+package com.yapp.web2.controller.docs
+
+import com.yapp.web2.controller.ApiControllerTest
+import org.junit.jupiter.api.Test
+
+class RestDocsControllerTest : ApiControllerTest(uri = "/api/v1/docs") {
+
+	@Test
+	fun docsSimpleTest() {
+	}
+
+}
