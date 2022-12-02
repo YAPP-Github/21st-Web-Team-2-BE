c3b652dd1f58a10ab1d9d439d4f3895c8a313c4d
Jimin Lim <50178026+jimin3263@users.noreply.github.com>
2022-12-02 12:40:10 +0900

    🔧 jacoco, codecov 적용 (#6)
    * :wrench: jacoco 설정
    
    * :wrench: codecov 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.0"
-	id("io.spring.dependency-management") version "1.1.0"
-	id("org.asciidoctor.jvm.convert") version "3.3.2"
-	kotlin("jvm") version "1.7.21"
-	kotlin("plugin.spring") version "1.7.21"
-	kotlin("plugin.jpa") version "1.7.21"
+    id("org.springframework.boot") version "3.0.0"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
+    kotlin("jvm") version "1.7.21"
+    kotlin("plugin.spring") version "1.7.21"
+    kotlin("plugin.jpa") version "1.7.21"
+    jacoco
 }
 
 group = "com.yapp"
@@ -14,83 +15,132 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("com.coveo:spring-boot-parameter-store-integration:1.5.0")
-	runtimeOnly("com.h2database:h2")
-	runtimeOnly("com.mysql:mysql-connector-j")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	// Spring Rest Docs
-	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
-	testImplementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.coveo:spring-boot-parameter-store-integration:1.5.0")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    // Spring Rest Docs
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    testImplementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
+}
+
+jacoco {
+    toolVersion = "0.8.8"
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+    finalizedBy("jacocoTestReport")
 }
 
 tasks.getByName<Jar>("jar") {
     enabled = false
 }
+
 tasks {
-	val snippetsDir = file("$buildDir/generated-snippets")
+    val snippetsDir = file("$buildDir/generated-snippets")
 
-	clean {
-		delete("src/main/resources/static/docs")
-	}
+    clean {
+        delete("src/main/resources/static/docs")
+    }
 
-	test {
-		useJUnitPlatform()
-		systemProperty("org.springframework.restdocs.outputDir", snippetsDir)
-		outputs.dir(snippetsDir)
-	}
+    test {
+        useJUnitPlatform()
+        systemProperty("org.springframework.restdocs.outputDir", snippetsDir)
+        outputs.dir(snippetsDir)
+    }
 
-	build {
-		dependsOn("copyDocument")
-	}
+    build {
+        dependsOn("copyDocument")
+    }
 
-	asciidoctor {
-		dependsOn(test)
+    asciidoctor {
+        dependsOn(test)
 
-		attributes(
-			mapOf("snippets" to snippetsDir)
-		)
-		inputs.dir(snippetsDir)
+        attributes(
+                mapOf("snippets" to snippetsDir)
+        )
+        inputs.dir(snippetsDir)
 
-		doFirst {
-			delete("src/main/resources/static/docs")
-		}
-	}
+        doFirst {
+            delete("src/main/resources/static/docs")
+        }
+    }
 
-	register<Copy>("copyDocument") {
-		dependsOn(asciidoctor)
+    register<Copy>("copyDocument") {
+        dependsOn(asciidoctor)
 
-		destinationDir = file(".")
-		from(asciidoctor.get().outputDir) {
-			into("src/main/resources/static/docs")
-		}
-	}
+        destinationDir = file(".")
+        from(asciidoctor.get().outputDir) {
+            into("src/main/resources/static/docs")
+        }
+    }
 
-	bootJar {
-		dependsOn(asciidoctor)
+    bootJar {
+        dependsOn(asciidoctor)
 
-		from(asciidoctor.get().outputDir) {
-			into("BOOT-INF/classes/static/docs")
-		}
-	}
+        from(asciidoctor.get().outputDir) {
+            into("BOOT-INF/classes/static/docs")
+        }
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+        csv.required.set(false)
+    }
+
+    classDirectories.setFrom(
+            sourceSets.main.get().output.asFileTree.matching {
+                exclude("**/Web2ApplicationKt*")
+            }
+    )
+
+    finalizedBy("jacocoTestCoverageVerification")
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            enabled = true
+            element = "CLASS"
+
+            limit {
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                minimum = "0.80".toBigDecimal()
+            }
+        }
+    }
+}
+
+val testCoverage by tasks.registering {
+    group = "verification"
+    description = "Runs the unit tests with coverage"
+
+    dependsOn(":test",
+            ":jacocoTestReport",
+            ":jacocoTestCoverageVerification")
+
+    tasks["jacocoTestReport"].mustRunAfter(tasks["test"])
+    tasks["jacocoTestCoverageVerification"].mustRunAfter(tasks["jacocoTestReport"])
 }
