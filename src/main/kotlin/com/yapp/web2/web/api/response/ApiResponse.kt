4b19f0298d0ea38a2e7bf63797802a1a6c9ba936
limjimin <dkdldsla0000@naver.com>
2022-12-04 03:18:22 +0900

    :sparkles: 응답 코드 작성

---
+++ src/main/kotlin/com/yapp/web2/web/api/response/ApiResponse.kt
@@ -0,0 +1,54 @@
+package com.yapp.web2.web.api.response
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.yapp.web2.web.api.error.ErrorCode
+import org.springframework.data.domain.Slice
+
+data class ApiResponse<T>(
+    val code: String,
+
+    val message: String,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    val data: T?,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    val pageSize: Int? = null,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    val pageNumber: Int? = null,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    val hasNext: Boolean? = null,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    val offsetId: Long? = null,
+) {
+    companion object {
+        fun success() = ApiResponse(
+            code = "SUCCESS",
+            message = "성공",
+            data = null,
+        )
+
+        fun <T> success(data: T) = ApiResponse(
+            code = "SUCCESS",
+            message = "성공",
+            data = data,
+        )
+
+        fun <T : OffsetIdSupport> success(slice: Slice<T>) = ApiResponse(
+            code = "SUCCESS",
+            message = "성공",
+            data = slice.content,
+            hasNext = slice.hasNext(),
+            offsetId = if (!slice.hasContent()) null else slice.content.last().offsetId,
+        )
+
+        fun failure(errorCode: ErrorCode) = ApiResponse<ErrorCode>(
+            code = errorCode.code,
+            message = errorCode.message,
+            data = null,
+        )
+    }
+}
