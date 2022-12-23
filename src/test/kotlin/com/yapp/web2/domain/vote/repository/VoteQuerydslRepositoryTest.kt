a1d7ccabf12eedec419f0945a0d193987fc07a73
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-12-23 23:40:57 +0900

    :bug: 순서에 의존적인 테스트 수정

--- src/test/kotlin/com/yapp/web2/domain/vote/repository/VoteQuerydslRepositoryTest.kt
+++ src/test/kotlin/com/yapp/web2/domain/vote/repository/VoteQuerydslRepositoryTest.kt
@@ -6,6 +6,7 @@ import com.yapp.web2.domain.member.repository.MemberRepository
 import com.yapp.web2.domain.vote.model.Vote
 import com.yapp.web2.domain.vote.model.VoteType
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -16,41 +17,64 @@ import org.springframework.transaction.annotation.Transactional
 @SpringBootTest
 internal class VoteQuerydslRepositoryTest @Autowired constructor(
     val voteRepository: VoteRepository,
-    val voteQuerydslRepository: VoteQuerydslRepository,
     val memberRepository: MemberRepository,
+    val voteQuerydslRepository: VoteQuerydslRepository,
 ) {
+
+    @BeforeEach
+    fun beforeEach() {
+        memberRepository.deleteAll()
+        voteRepository.deleteAll()
+    }
+
     @Test
     fun `투표 페이징 조회 테스트`() {
-        saveVotes(30)
+        //given
+        val dummyVoteAmount = 20
+        val pageSize = 6
 
-        val searchBySlice = voteQuerydslRepository.searchBySlice(pageable = Pageable.ofSize(3))
+        val dummyVotes = saveDummyVotes(dummyVoteAmount)
+        dummyVotes.sortByDescending { it.createdAt }
+
+        //when
+        val searchBySlice = voteQuerydslRepository.findVotePreviewsLessThanId(pageable = Pageable.ofSize(pageSize))
+
+        //then
         val content = searchBySlice?.content
-        assertThat(content).hasSize(3)
-        assertThat(content!![0].title).isEqualTo("Vote30")
-        assertThat(searchBySlice.hasNext()).isTrue
+        assertThat(content).hasSize(pageSize)
+        assertThat(searchBySlice!!.hasNext()).isTrue
     }
 
     @Test
     fun `투표 페이징 인덱스 조회 테스트`() {
-        saveVotes(30)
+        //given
+        val dummyVoteAmount = 20
+        val pageSize = 5
 
-        // 최신순 -> id: 30, 29, 28, ... ,2, 1 순으로 정렬됨
-        // id가 3번 이후 부터 size = 3으로 slice
-        val searchBySlice = voteQuerydslRepository.searchBySlice(3, Pageable.ofSize(3))
+        val dummyVotes = saveDummyVotes(dummyVoteAmount)
+        dummyVotes.sortByDescending { it.createdAt }
+        val lastVoteId = dummyVotes[(dummyVoteAmount - 1) - 2].id
+
+        //when
+        //우선순위(최신순)으로 정렬된 데이터에서, id가 lastVoteId 이후에서 부터 조회
+        val searchBySlice = voteQuerydslRepository.findVotePreviewsLessThanId(lastVoteId, Pageable.ofSize(pageSize))
+
+        //then
         val content = searchBySlice?.content
         assertThat(content).hasSize(2)
         assertThat(searchBySlice!!.hasNext()).isFalse
     }
 
     //test용 투표 저장
-    private fun saveVotes(amount: Int) {
+    private fun saveDummyVotes(amount: Int): MutableList<Vote> {
         val member = Member("MemberA", JobCategory.DEVELOPER, 3)
         memberRepository.save(member)
         val sampleVotes: MutableList<Vote> = mutableListOf()
         for (i in 1..amount) {
             sampleVotes.add(Vote("Vote$i", JobCategory.DEVELOPER, "Content$i", VoteType.TEXT, createdBy = member))
         }
-        voteRepository.saveAll(sampleVotes)
+
+        return voteRepository.saveAll(sampleVotes)
     }
 
 }
