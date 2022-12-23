1d095bd04f66823b974bdd0ead03a7eb78cf12d1
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-12-23 23:31:41 +0900

    :recycle: 메서드 이름 수정

--- src/main/kotlin/com/yapp/web2/domain/vote/repository/VoteQuerydslRepository.kt
+++ src/main/kotlin/com/yapp/web2/domain/vote/repository/VoteQuerydslRepository.kt
@@ -13,32 +13,24 @@ import org.springframework.stereotype.Component
 class VoteQuerydslRepository(
     private val queryFactory: JPAQueryFactory
 ) {
-
-    fun searchBySlice(lastVoteId: Long? = null, pageable: Pageable): Slice<Vote>? {
+    fun findVotePreviewsLessThanId(lastVoteId: Long? = null, pageable: Pageable): Slice<Vote>? {
         val results: MutableList<Vote> = queryFactory.selectFrom(vote)
-            .where(ltVoteId(lastVoteId)) //no-offset paging
+            .where(lastVoteId?.let {
+                vote.id.lt(lastVoteId)
+            })
             .orderBy(vote.createdAt.desc())
             .limit((pageable.pageSize + 1).toLong())
             .fetch()
 
-        return checkLastPage(pageable, results)
+        return SliceImpl(results, pageable, hasNext(pageable, results))
     }
 
-    private fun ltVoteId(voteId: Long?): BooleanExpression? {
-        return voteId?.let {
-            vote.id.lt(voteId)
-        }
-    }
-
-    // 무한 스크롤 방식 처리하는 메서드
-    private fun checkLastPage(pageable: Pageable, results: MutableList<Vote>): Slice<Vote> {
-        var hasNext = false
-
-        // 조회한 결과 개수가 요청한 페이지 사이즈보다 크면 뒤에 더 있음, next = true
-        if (results.size > pageable.pageSize) {
-            hasNext = true
+    private fun hasNext(pageable: Pageable, results: MutableList<Vote>): Boolean {
+        return if (results.size > pageable.pageSize) {
             results.removeAt(pageable.pageSize)
+            true
+        } else {
+            false
         }
-        return SliceImpl(results, pageable, hasNext)
     }
 }
