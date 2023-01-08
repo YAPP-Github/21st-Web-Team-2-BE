55915f313dd42533f8e530e7d2b8286c7993a17c
limjimin <dkdldsla0000@naver.com>
2023-01-09 01:19:49 +0900

    :sparkles: jwt 기능 추가

---
+++ src/main/kotlin/com/yapp/web2/domain/jwt/application/JwtService.kt
@@ -0,0 +1,27 @@
+package com.yapp.web2.domain.jwt.application
+
+import com.yapp.web2.domain.jwt.util.JwtProvider
+import com.yapp.web2.domain.member.application.MemberService
+import com.yapp.web2.web.api.dto.jwt.response.JwtTokens
+import com.yapp.web2.web.api.error.BusinessException
+import com.yapp.web2.web.api.error.ErrorCode
+import org.springframework.stereotype.Service
+
+@Service
+class JwtService(
+    private val jwtProvider: JwtProvider,
+    private val memberService: MemberService,
+) {
+    fun issue(email: String): JwtTokens {
+        memberService.findByEmail(email)
+            ?: throw BusinessException(ErrorCode.MEMBER_NOT_FOUND)
+
+        val accessToken: String = jwtProvider.createAccessToken(email)
+        val refreshToken: String = jwtProvider.createRefreshToken()
+
+        return JwtTokens(
+            accessToken = accessToken,
+            refreshToken = refreshToken
+        )
+    }
+}
