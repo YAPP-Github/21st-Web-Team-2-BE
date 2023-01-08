80daa27b20ba16a1c21157353ba0d0202b2bd56a
limjimin <dkdldsla0000@naver.com>
2023-01-09 01:26:33 +0900

    :truck: dto 위치 변경

--- src/main/kotlin/com/yapp/web2/domain/jwt/application/JwtService.kt
+++ src/main/kotlin/com/yapp/web2/domain/jwt/application/JwtService.kt
@@ -2,7 +2,7 @@ package com.yapp.web2.domain.jwt.application
 
 import com.yapp.web2.domain.jwt.util.JwtProvider
 import com.yapp.web2.domain.member.application.MemberService
-import com.yapp.web2.web.api.dto.jwt.response.JwtTokens
+import com.yapp.web2.web.dto.jwt.response.JwtTokens
 import com.yapp.web2.web.api.error.BusinessException
 import com.yapp.web2.web.api.error.ErrorCode
 import org.springframework.stereotype.Service
