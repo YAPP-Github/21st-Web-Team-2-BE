36c3a4dd412d9b7c1df25136c889f2f42eb1403c
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-01-15 15:55:15 +0900

    :recycle: VoteOptionId 포함하여 응답하도록 수정

--- src/main/kotlin/com/yapp/web2/web/dto/voteoption/response/VoteOptionPreviewResponse.kt
+++ src/main/kotlin/com/yapp/web2/web/dto/voteoption/response/VoteOptionPreviewResponse.kt
@@ -3,6 +3,7 @@ package com.yapp.web2.web.dto.voteoption.response
 import com.yapp.web2.domain.topic.model.option.VoteOption
 
 data class VoteOptionPreviewResponse(
+    val id: Long,
     val text: String?,
     val voteOptionImageFilename: String?,
     val codeBlock: String?,
@@ -13,6 +14,7 @@ data class VoteOptionPreviewResponse(
     companion object {
         fun of(voteOption: VoteOption, voted: Boolean = false): VoteOptionPreviewResponse {
             return VoteOptionPreviewResponse(
+                voteOption.id,
                 voteOption.text,
                 voteOption.voteOptionImageFilename,
                 voteOption.codeBlock,
