73b67e798533e484b9952870b6a7f6f6fd33885a
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-01 14:45:51 +0900

    :sparkles: 댓글 좋아요 로직 추가

--- src/main/kotlin/com/yapp/web2/domain/like/repository/CommentLikesRepository.kt
+++ src/main/kotlin/com/yapp/web2/domain/like/repository/CommentLikesRepository.kt
@@ -1,6 +1,12 @@
 package com.yapp.web2.domain.like.repository
 
+import com.yapp.web2.domain.comment.model.Comment
 import com.yapp.web2.domain.like.model.CommentLikes
+import com.yapp.web2.domain.like.model.TopicLikes
+import com.yapp.web2.domain.member.model.Member
+import com.yapp.web2.domain.topic.model.Topic
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CommentLikesRepository : JpaRepository<CommentLikes, Long>
+interface CommentLikesRepository : JpaRepository<CommentLikes, Long> {
+    fun findByLikedByAndComment(member: Member, topic: Comment): CommentLikes?
+}
