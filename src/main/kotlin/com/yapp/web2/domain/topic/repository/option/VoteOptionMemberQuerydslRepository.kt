415da84b04247aa5cca9ac2dc50d759faccc0aeb
Taeyeon <90550065+TaeyeonRoyce@users.noreply.github.com>
2023-02-22 23:57:14 +0900

    🐛 투표 이력 조회시, 유저에 대한 전체 조건이 아닌 투표 게시글에 대한 조건만 반영되도록 수정 (#81)

--- src/main/kotlin/com/yapp/web2/domain/topic/repository/option/VoteOptionMemberQuerydslRepository.kt
+++ src/main/kotlin/com/yapp/web2/domain/topic/repository/option/VoteOptionMemberQuerydslRepository.kt
@@ -2,7 +2,6 @@ package com.yapp.web2.domain.topic.repository.option
 
 import com.querydsl.jpa.impl.JPAQueryFactory
 import com.yapp.web2.domain.member.model.Member
-import com.yapp.web2.domain.topic.model.QTopic.topic
 import com.yapp.web2.domain.topic.model.Topic
 import com.yapp.web2.domain.topic.model.option.QVoteOptionMember.voteOptionMember
 import com.yapp.web2.domain.topic.model.option.VoteOptionMember
@@ -15,8 +14,7 @@ class VoteOptionMemberQuerydslRepository(
 
     fun findByMemberAndTopic(votedBy: Member, votedTopic: Topic): VoteOptionMember? {
         return queryFactory.selectFrom(voteOptionMember)
-            .leftJoin(topic).on(voteOptionMember.voteOption.topic.eq(votedTopic))
-            .where(voteOptionMember.votedBy.eq(votedBy))
+            .where(voteOptionMember.votedBy.eq(votedBy), voteOptionMember.voteOption.topic.eq(votedTopic))
             .fetchOne()
     }
 }
